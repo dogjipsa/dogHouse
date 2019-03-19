b1bedb86a1d79275c9b96b5ee5d48df699c675e5
Ssoo29 <fereverv@naver.com>
2019-03-19 21:30:40 +0900

    ㄴ

---
+++ animalProject/src/freeboard/controller/freeBoardDeleteServlet.java
@@ -0,0 +1,54 @@
+package freeboard.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import freeboard.model.service.FreeBoardService;
+
+
+/**
+ * Servlet implementation class BoardDeleteServlet
+ */
+@WebServlet("/fdelete")
+public class freeBoardDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public freeBoardDeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//게시글 (원글, 댓글) 삭제 처리용 컨트롤러
+		int boardNum = Integer.parseInt(request.getParameter("bnum"));
+		
+		/*if(new FreeBoardService().deleteFreeBoard(boardNum) > 0) */
+			response.sendRedirect("/doggybeta/blist?");
+		/*}else {
+			RequestDispatcher view = request.getRequestDispatcher("views/freeboard/freeBoardError.jsp");
+			request.setAttribute("message", boardNum + "번 게시글 삭제 실패!");
+			view.forward(request, response);
+		}*/
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
